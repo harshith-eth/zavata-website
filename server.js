e973c29f4d1253c1ab42a36639c347c6c018ec1f
harshith-eth <vharshith.2810@gmail.com>
2025-02-23 16:41:58 +0530

    Initial commit: Updated branding from Zavata to Human01D

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/blogs', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'blogs', 'blogs.html'));
+});
+
+app.get('/terms-of-service', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'terms-of-service.html'));
+});
+
+app.get('/ai-ethics-policy', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'ai-ethics-policy.html'));
+});
+
+app.get('/cookie-policy', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'cookie-policy.html'));
+});
+
+app.get('/privacy-policy', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'privacy-policy.html'));
+});
+
+app.get('/blogs/human01d-launch', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'blogs', 'blog1', 'launch.html'));
+});
+
+
+// Additional routes for new blog entries
+app.get('/blogs/msft4startups', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'blogs', 'blog2', 'msft.html'));
+});
+
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
